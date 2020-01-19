1d9593c21f334100694be17157324c5c31290c26
KESHAOYE <517034774@qq.com>
2020-01-19 22:32:34 +0800

    feat: 添加sort 20.01.19 alpha 1

--- routes/home/sort.js
+++ routes/home/sort.js
@@ -1,10 +1,36 @@
 const express = require("express");
-const graphql = require('graphql');
-const egraph = require('express-graphql')
 const mysql = require('../../util/db')
 const app = express();
 const token = require('../../util/token')
 
+// 获取分类
+app.use('/GETSORT', (req, res, next) => {
+    let { count } = req.body | '15'
+    let sql = `select * from sortinfo left join shopinfo on sortinfo.sortid = shopinfo.shopsort`
+    let result = []
+    mysql(sql)
+        .then(data => {
+            data.forEach(el => {
+                let a = {
+                    sortid: el.sortid,
+                    sortname: el.sortname,
+                    sortename: el.sortename,
+                    isshow: el.isshow,
+                    data: []
+                }
+            })
+            res.json({
+                code: 200,
+                status: true,
+            })
+        })
+        .catch(err => {
+            res.json({
+                code: 600,
+                message: err
+            })
+        })
+})
 
 var schema = graphql.buildSchema(`
  type sort{
@@ -31,55 +57,55 @@ type Mutation{
 `)
 
 var root = {
-  getsort(){
-    return new Promise((resolve, reject)=>{
-    mysql.query('select * from shopinfo where sorid = (select * from sortInfo)',(err,data)=>{
-        if(err){
-          reject(new Error(err))
-        }else{
-            resolve(data)
+    getsort() {
+        return new Promise((resolve, reject) => {
+            mysql.query('select * from shopinfo where sorid = (select * from sortInfo)', (err, data) => {
+                if (err) {
+                    reject(new Error(err))
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+    },
+    insertSort({ input }) {
+        const data = {
+            sortname: input.sortname,
+            sortename: input.sortename,
+            createTime: new Date(),
+            isshow: input.isshow
         }
-    })
-    })  
-  },
-  insertSort({input}){
-    const data = {
-      sortname: input.sortname,
-      sortename: input.sortename,
-      createTime: new Date(),
-      isshow: input.isshow
-    }
-    return new Promise((resolve,reject)=>{
-      mysql.query('insert into sortInfo set ?',data, (err,data)=>{
-          if(err){
-              reject(new Error(err))
-          }else{
-              resolve(data)
-          }
-      })
-    })
-  },
-  updateSort(id,{input}){
-    const data = {
-      sortname: input.sortname,
-      sortename: input.sortename,
-      createTime: input.createTime,
-      isshow: input.isshow
-    }
-    return new Promise((resolve,reject)=>{
-      mysql.query('update sortInfo set ? where ?',[data,id],(err,data)=>{
-          if(err){
-             reject(new Error(err))
-          }else{
-           resolve(data)    
+        return new Promise((resolve, reject) => {
+            mysql.query('insert into sortInfo set ?', data, (err, data) => {
+                if (err) {
+                    reject(new Error(err))
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+    },
+    updateSort(id, { input }) {
+        const data = {
+            sortname: input.sortname,
+            sortename: input.sortename,
+            createTime: input.createTime,
+            isshow: input.isshow
         }
-      })
-    })
-  }
+        return new Promise((resolve, reject) => {
+            mysql.query('update sortInfo set ? where ?', [data, id], (err, data) => {
+                if (err) {
+                    reject(new Error(err))
+                } else {
+                    resolve(data)
+                }
+            })
+        })
+    }
 }
 
-router.post('/',(req,res,next)=>{
-   console.log(req.query)
+router.post('/', (req, res, next) => {
+    console.log(req.query)
 })
 
 module.exports = app
